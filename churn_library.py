82d131c59fbfaf91afc773623dc0c706688bf64a
Patrick Schmidt <patschmidt1234@gmail.com>
2022-10-12 15:08:54 +0200

    pylint cleanup churn_library

--- churn_library.py
+++ churn_library.py
@@ -1,13 +1,16 @@
-# library doc string
 '''
 This module contains functions to perform data analysis and machine learning on
-the Kaggle Credit Card customers dataset to predict customers who are likely to churn.
+the Kaggle Credit Card customers dataset to predict customers
+who are likely to churn.
 
 Author: Patrick
 Date: October 2022
 '''
-
 # import libraries
+import os
+import logging
+import matplotlib
+import matplotlib.pyplot as plt
 from sklearn.metrics import RocCurveDisplay, classification_report
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.linear_model import LogisticRegression
@@ -15,12 +18,8 @@ from sklearn.ensemble import RandomForestClassifier
 import seaborn as sns
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import os
-import logging
 import joblib
 import shap
-import matplotlib
 import constants
 
 
@@ -58,6 +57,8 @@ def import_data(input_pth):
         return input_df
     except FileNotFoundError:
         print_and_log(f"Could not read file {input_pth}", logging.error)
+        return None
+
 
 
 def perform_eda(input_df):
@@ -127,15 +128,15 @@ def perform_eda(input_df):
     print_and_log("EDA finished.", logging.info)
 
 
-def encoder_helper(input_df, category_lst, response):
+def encoder_helper(input_df, category_lst):
     '''
     helper function to turn each categorical column into a new column with
-    propotion of churn for each category - associated with cell 15 from the notebook
+    propotion of churn for each category -
+    associated with cell 15 from the notebook
 
     input:
             df: pandas dataframe
             category_lst: list of columns that contain categorical features
-            response: string of response name [optional argument that could be used for naming variables or index y column]
 
     output:
             df: pandas dataframe with new columns for
@@ -159,113 +160,125 @@ def encoder_helper(input_df, category_lst, response):
     return input_df
 
 
-def perform_feature_engineering(input_df, response):
+def perform_feature_engineering(input_df):
     '''
     input:
               input_df: pandas dataframe
-              response: string of response name [optional argument that could be used for naming variables or index y column]
 
     output:
-              X_train: X training data
-              X_test: X testing data
-              y_train: y training data
-              y_test: y testing data
+              features_train: X training data
+              features_test: X testing data
+              targets_train: y training data
+              targets_test: y testing data
     '''
-
     print_and_log("Performing feature engineering", logging.info)
-    input_df = encoder_helper(input_df, constants.CAT_COLUMNS, None)
+    input_df = encoder_helper(input_df, constants.CAT_COLUMNS)
 
     # Empty dataframe for features
-    X = pd.DataFrame()
-    y = input_df['Churn']
+    features_df = pd.DataFrame()
+    target_df = input_df['Churn']
 
-    
-    X[constants.KEEP_COLS] = input_df[constants.KEEP_COLS]
+    features_df[constants.KEEP_COLS] = input_df[constants.KEEP_COLS]
 
-    X.head()
+    features_df.head()
 
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.3, random_state=42)
+    (features_train, features_test,
+    targets_train, targets_test) = train_test_split(
+        features_df, target_df, test_size=0.3, random_state=42)
 
     print_and_log("Performed feature engineering", logging.info)
 
-    return X_train, X_test, y_train, y_test
+    return features_train, features_test, targets_train, targets_test
 
 
-def classification_report_image(y_train,
-                                y_test,
-                                y_train_preds_lr,
-                                y_train_preds_rf,
-                                y_test_preds_lr,
-                                y_test_preds_rf):
+def classification_report_image(targets_train,
+                                targets_test,
+                                targets_train_preds_lr,
+                                targets_train_preds_rf,
+                                targets_test_preds_lr,
+                                targets_test_preds_rf):
     '''
-    produces classification report for training and testing results and stores report as image
-    in images folder
+    produces classification report for training and testing results
+    and stores report as image in images folder
     input:
-            y_train: training response values
-            y_test:  test response values
-            y_train_preds_lr: training predictions from logistic regression
-            y_train_preds_rf: training predictions from random forest
-            y_test_preds_lr: test predictions from logistic regression
-            y_test_preds_rf: test predictions from random forest
+            targets_train: training response values
+            targets_test:  test response values
+            targets_train_preds_lr: training predictions
+                                    from logistic regression
+            targets_train_preds_rf: training predictions from random forest
+            targets_test_preds_lr: test predictions from logistic regression
+            targets_test_preds_rf: test predictions from random forest
 
     output:
              None
     '''
+    # Do the classification report
+    cls_report_test_preds_rf = classification_report(targets_test,
+                                                    targets_test_preds_rf)
+    cls_report_train_preds_rf = classification_report(targets_train,
+                                                    targets_train_preds_rf)
+    cls_report_test_preds_lr = classification_report(targets_test,
+                                                    targets_test_preds_lr)
+    cls_report_train_preds_lr = classification_report(targets_train,
+                                                    targets_train_preds_lr)
+
     print_and_log('random forest results', logging.info)
     print_and_log('test results', logging.info)
     print_and_log(
-        "\n" + str(classification_report(y_test, y_test_preds_rf)), logging.info)
+        "\n" + str(cls_report_test_preds_rf), logging.info)
     print_and_log('train results', logging.info)
     print_and_log(
-        "\n" + str(classification_report(y_train, y_train_preds_rf)), logging.info)
+        "\n" + str(cls_report_train_preds_rf), logging.info)
 
     print_and_log('logistic regression results', logging.info)
     print_and_log('test results', logging.info)
     print_and_log(
-        "\n" + str(classification_report(y_test, y_test_preds_lr)), logging.info)
+        "\n" + str(cls_report_test_preds_lr), logging.info)
     print_and_log('train results', logging.info)
     print_and_log(
-        "\n" + str(classification_report(y_train, y_train_preds_lr)), logging.info)
+        "\n" + str(cls_report_train_preds_lr), logging.info)
 
     # again, but saving it as a figure.
-    # TODO save the classification report in a local variable to avoid double
-    # computation
     print_and_log("Creating graphical classification report", logging.info)
     plt.figure(figsize=(7, 5))
-    plt.text(0.01, 1.25, str('Random Forest Train'), {
+    plt.text(0.01, 1.25, 'Random Forest Train', {
              'fontsize': 10}, fontproperties='monospace')
-    plt.text(0.01, 0.05, str(classification_report(y_test, y_test_preds_rf)), {
-             'fontsize': 10}, fontproperties='monospace')  # approach improved by OP -> monospace!
+    plt.text(0.01, 0.05, str(cls_report_train_preds_rf), {
+             'fontsize': 10}, fontproperties='monospace')
     plt.text(0.01, 0.6, str('Random Forest Test'), {
              'fontsize': 10}, fontproperties='monospace')
-    plt.text(0.01, 0.7, str(classification_report(y_train, y_train_preds_rf)), {
-             'fontsize': 10}, fontproperties='monospace')  # approach improved by OP -> monospace!
+    plt.text(0.01, 0.7, str(cls_report_test_preds_rf), {
+             'fontsize': 10}, fontproperties='monospace')
     plt.axis('off')
+    # Make text fit
     plt.tight_layout()
+    # Save to disk
     plt.savefig(constants.CLS_REPORT_PLT_PTH)
     print_and_log(
         f"Saved graphical report to {constants.CLS_REPORT_PLT_PTH}",
         logging.info)
 
+    # Creating ROC curves for RFC and LRC
     print_and_log("Creating ROC curves", logging.info)
-    lrc_plot = RocCurveDisplay.from_predictions(y_test, y_test_preds_lr)
-    ax = plt.gca()
-    rfc_disp = RocCurveDisplay.from_predictions(y_test, y_test_preds_rf,
-                                                ax=ax, alpha=0.8)
+    _ = RocCurveDisplay.from_predictions(targets_test, targets_test_preds_lr)
+    roc_ax = plt.gca()
+    _ = RocCurveDisplay.from_predictions(targets_test, targets_test_preds_rf,
+                                                ax=roc_ax, alpha=0.8)
+    # Make plot fit to figure
     plt.tight_layout()
+    # Save to disk
     plt.savefig(constants.ROC_CURVE_PLT_PTH)
     print_and_log(
         f"Saved ROC curve plot to {constants.ROC_CURVE_PLT_PTH}",
         logging.info)
 
 
-def feature_importance_plot(model, X_data, output_pth, shap_output_pth):
+def feature_importance_plot(model, features_data, output_pth, shap_output_pth):
     '''
     creates and stores the feature importances in pth
     input:
             model: model object containing feature_importances_
-            X_data: pandas dataframe of X values
+            features_data: pandas dataframe of X values
             output_pth: Path for the feature importance plot to save
             shap_output_pth: Path for the shap feature importance plot to save
 
@@ -274,9 +287,9 @@ def feature_importance_plot(model, X_data, output_pth, shap_output_pth):
     '''
     print_and_log("Running SHAP analysis...", logging.info)
     explainer = shap.TreeExplainer(model)
-    shap_values = explainer.shap_values(X_data)
+    shap_values = explainer.shap_values(features_data)
     plt.figure(figsize=(15, 10))
-    shap.summary_plot(shap_values, X_data, plot_type="bar")
+    shap.summary_plot(shap_values, features_data, plot_type="bar")
 
     # Make labels fit
     plt.tight_layout()
@@ -289,7 +302,7 @@ def feature_importance_plot(model, X_data, output_pth, shap_output_pth):
     indices = np.argsort(importances)[::-1]
 
     # Rearrange feature names so they match the sorted feature importances
-    names = [X_data.columns[i] for i in indices]
+    names = [features_data.columns[i] for i in indices]
 
     # Create plot
     plt.figure(figsize=(20, 10))
@@ -299,10 +312,10 @@ def feature_importance_plot(model, X_data, output_pth, shap_output_pth):
     plt.ylabel('Importance')
 
     # Add bars
-    plt.bar(range(X_data.shape[1]), importances[indices])
+    plt.bar(range(features_data.shape[1]), importances[indices])
 
     # Add feature names as x-axis labels
-    plt.xticks(range(X_data.shape[1]), names, rotation=90)
+    plt.xticks(range(features_data.shape[1]), names, rotation=90)
 
     # Make the labels fit
     plt.tight_layout()
@@ -313,24 +326,24 @@ def feature_importance_plot(model, X_data, output_pth, shap_output_pth):
         logging.info)
 
 
-def test_models(X_train, X_test, model_pth_rf, model_pth_lr):
+def test_models(features_train, features_test, model_pth_rf, model_pth_lr):
     '''
     test models after completed training
 
     Input:
-        X_train: X training data
-        X_test: X testing data
+        features_train: X training data
+        features_test: X testing data
         model_rf: random forest model
         model_lr: logistic regression model
 
     Output:
-        y_train_preds_rf: Prediction result for the random forest classifier
+        targets_train_preds_rf: Prediction result for the
+                            random forest classifier on the train split
+        targets_test_preds_rf: Prediction result for the
+                            random forest classifier on the test split
+        targets_train_preds_lr: Prediction result for the log reg classifier
                             on the train split
-        y_test_preds_rf: Prediction result for the random forest classifier
-                            on the test split
-        y_train_preds_lr: Prediction result for the log reg classifier
-                            on the train split
-        y_test_preds_lr: Prediction result for the log reg classifier
+        targets_test_preds_lr: Prediction result for the log reg classifier
                             on the test split
 
     '''
@@ -342,31 +355,32 @@ def test_models(X_train, X_test, model_pth_rf, model_pth_lr):
     print_and_log(f"Loading model from path {model_pth_lr}", logging.info)
     lrc = joblib.load(model_pth_lr)
 
+    # Do prediction for random forest
     print_and_log(
         "Predicting values with random forest classifier",
         logging.info)
-    # Do prediction for random forest
-    y_train_preds_rf = rfc.predict(X_train)
-    y_test_preds_rf = rfc.predict(X_test)
+    targets_train_preds_rf = rfc.predict(features_train)
+    targets_test_preds_rf = rfc.predict(features_test)
 
+    # Do prediction for logistic regression
     print_and_log(
         "Predicting values from logistic regression classifier",
         logging.info)
-    # Do prediction for logistic regression
-    y_train_preds_lr = lrc.predict(X_train)
-    y_test_preds_lr = lrc.predict(X_test)
+    targets_train_preds_lr = lrc.predict(features_train)
+    targets = lrc.predict(features_test)
 
-    return y_train_preds_rf, y_test_preds_rf, y_train_preds_lr, y_test_preds_lr
+    return (targets_train_preds_rf, targets_test_preds_rf,
+            targets_train_preds_lr, targets)
 
 
-def train_models(X_train, X_test, y_train, y_test):
+def train_models(features_train, features_test, targets_train, targets_test):
     '''
     train, store model results: images + scores, and store models
     input:
-              X_train: X training data
-              X_test: X testing data
-              y_train: y training data
-              y_test: y testing data
+              features_train: X training data
+              features_test: X testing data
+              targets_train: y training data
+              targets_test: y testing data
     output:
               None
     '''
@@ -396,12 +410,12 @@ def train_models(X_train, X_test, y_train, y_test):
         param_grid=param_grid,
         cv=5,
         n_jobs=12)
-    cv_rfc.fit(X_train, y_train)
+    cv_rfc.fit(features_train, targets_train)
     print_and_log("Finished grid search of rf classifier", logging.info)
 
     # Logistic regression classifier training
     print_and_log("Training LR classifier", logging.info)
-    lrc.fit(X_train, y_train)
+    lrc.fit(features_train, targets_train)
     print_and_log("Trained LR classifier", logging.info)
 
     # Save best model
@@ -415,21 +429,24 @@ def train_models(X_train, X_test, y_train, y_test):
         logging.info)
 
     # Use the model. Formula is WX + b = y, used by the predict function
-    (y_train_preds_rf, y_test_preds_rf, y_train_preds_lr, y_test_preds_lr) = test_models(
-        X_train, X_test, constants.RFC_MODEL_PTH, constants.LR_MODEL_PTH)
+    (targets_train_preds_rf, targets_test_preds_rf,
+    targets_train_preds_lr, targets_test_preds_lr) = test_models(
+        features_train, features_test,
+        constants.RFC_MODEL_PTH, constants.LR_MODEL_PTH)
 
+    # Make the classification report
     classification_report_image(
-        y_train,
-        y_test,
-        y_train_preds_lr,
-        y_train_preds_rf,
-        y_test_preds_lr,
-        y_test_preds_rf)
+        targets_train,
+        targets_test,
+        targets_train_preds_lr,
+        targets_train_preds_rf,
+        targets_test_preds_lr,
+        targets_test_preds_rf)
 
     # Only do it with the random forest classifier because it SHAP does not
     # support Logistic Regression classifiers
     feature_importance_plot(
         cv_rfc.best_estimator_,
-        X_test,
+        features_test,
         constants.FEATURE_IMPORTANCE_PLT_RFC_PTH,
         constants.FEATURE_IMPORTANCE_SHAP_PLT_RFC_PTH)
